76b27be4c6aa7f97e4b3a6c7f2112a81f8832dc7
kristapsGr <grinvaldskristaps2@gmail.com>
2025-10-11 22:20:41 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,47 +1,25 @@
-// Šis ir mans pirmais javascript projekts
-console.log('Hello World');
+const pogaAtnemt = document.getElementById("pogaAtnemt");
+const pogaNotirit = document.getElementById("pogaNotirit");
+const pogaPieskaitit = document.getElementById("pogaPieskaitit");
+const skaititajs = document.getElementById("skaititajs");
+let skaits = 0;
 
-let vards = 'Kristaps';
-console.log(vards);
-// Mainīgie nevar būt keywords (let, if, var)
-// Nevar sākties ar ciparu (1name)
-// Nevar saturēt atstarpes vai svītriņas (-)
-
-let izveletsCipars = 0.3;
-izveletsCipars = 1;
-console.log(izveletsCipars);
-// Mainiga vertiba vair mainīties vēlāk
-
-const izveletsSkaitlis = 0.3;
-console.log(izveletsSkaitlis);
-// const neļauj mainiga vertibai mainīties
-
-let isAproved = true; // boolian
-let lastName = null; // notīra visa mainīgā vērtību
-
-let cilveks = {
-    vards: 'Jānis',
-    vecums: 30
-};
-cilveks.vards = 'Kārlis';
-cilveks['vards'] = 'Pēteris'; // Tas pats, kas rindiņu augstāk
-console.log(cilveks.vards);
-
-let izveletasKrasas = ['Sarkans', 'Zaļš'];
-izveletasKrasas[2] = 'Dzeltens'; // Pievieno konkrētu vērtību sarakstam
-console.log(izveletasKrasas[0]); // [2] - pasaka, kad no saraksta es vēlos tieši pirmo vērtību
-console.log(izveletasKrasas.length); // length - izvada sarsksta garumu
-
-// IZveido funkciju tiesi tapatas ka python
-function sveiciens(vards, uzvards){
-    console.log('Labvakar ' + vards + uzvards);
+pogaPieskaitit.onclick = function(){
+    skaits++;
+    skaititajs.textContent = skaits;
 }
-// izsauc funkiciju oievinojot mainigajam vertibu
-sveiciens('Andri ', 'Bērziņ');
-
-function kvadrats(skaitlis){
-    return skaitlis * skaitlis
+pogaAtnemt.onclick = function(){
+    skaits--;
+    skaititajs.textContent = skaits;
 }
+pogaNotirit.onclick = function(){
+    skaits = 0;
+    skaititajs.textContent = 0;
+}
+const manaPoga = document.getElementById("manaPoga");
+manaPoga.addEventListener('click', function(e) {
+    const vards = prompt('Kāds ir tavs vārds?');
+    console.log(vards)
+//    document.body.innerHTML = "<h1>Sveiki, " + vards + "!</h1>"
+});
 
-let cipars = kvadrats(2);
-console.log(cipars);
